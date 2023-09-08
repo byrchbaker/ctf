661d449edb73685a3bc00eaa4d205e3518a50c2b
Byrch Baker <62627217+byrchbaker@users.noreply.github.com>
2023-09-07 20:12:51 -0500

    disable inspect element?

--- js/auth.js
+++ js/auth.js
@@ -506,3 +506,25 @@ logout.addEventListener('click', (e) => {
         alert(errorMessage);
     });
 });
+
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
+
+document.onkeydown = (e) => {
+    if (e.key == 123) {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.key == 'U') {
+        e.preventDefault();
+    }
+};
